e11eb2dc42d9a6c7b6a125d7c123984f3fefbe23
prasicek <38466311+prasicek@users.noreply.github.com>
2018-05-16 11:22:34 +0200

    New version of code

--- GlacialSteadyStateProfile.py
+++ GlacialSteadyStateProfile.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def ISSfromFlux(q,fd=7.26e-5,fs=3.27,us=1):
+def dhdxfromFlux(q,fd=7.26e-5,fs=3.27,us=1):
     # calculate ice thickness and ice sueface slope
     a = fd/fs*us
     c = us
@@ -16,63 +16,65 @@ def ISSfromFlux(q,fd=7.26e-5,fs=3.27,us=1):
     
     H = -(1/(3*a))*(C+delta0/C)
 
-    S = (us/fs)**(1./3)*H**(-2./3)
+    dhdx = (us/fs)**(1./n)*H**((1-n)/n)
 
-    return [S,H]
+    return [dhdx,H]
 
 # set x domain and constants
 length = 5e4
-lengthfrac = 0.5 # set relative horizontal position of equilibrium line
+nx = 5e3
+dx = length/(nx-1)
+Exfrac = 0.5 # set relative horizontal position of equilibrium line
+Ex = length*Exfrac
+Expos = int(nx*Exfrac)
 
 betaX = -5e-5
 beta = 1e-3
 K = 1e-4 
 U = 1e-3
-l = 1
+l = 1.
+n = 3.
 ELA = 0
 
-fs = 3.27
-fd = 5.388e-5
+ro = 910.0
+g = 9.81 
+yr = 31556926.0
+Ad = 2.39e-24
+As = 1.46e-19
+fd = Ad*(ro*g)**n*yr
+fs = As*(ro*g)**n*yr
 
 # calculate steady state  
-x = np.arange(length)
+x = np.arange(1,nx-3)*dx
 us = (U/K)**(1./l)
-acc = betaX*(x-length*lengthfrac)
+B = betaX*(x-length*Exfrac)
 MBerr = 1e6        
         
 while MBerr > 1:
     
-    q = np.cumsum(acc) 
+    q = np.cumsum(B)*dx
 
-    S, H = ISSfromFlux(q,fd,fs,us)
-    S[np.isnan(S)] = 0
+    dhdx, H = dhdxfromFlux(q,fd,fs,us)
     
-    Zs = np.cumsum(S)
-    Zs = Zs.max()-Zs
-    ELApos = np.abs(acc).argmin()
-    Zs = Zs+(ELA-Zs[ELApos])
+    h = np.cumsum(dhdx)*dx
+    h = h.max()-h
+    h = h+(ELA-h[Expos])
     
-    accideal = beta*(Zs-ELA)
+    Bideal = beta*(h-ELA)
 
-    MBerr = np.sum(((acc-accideal)**2)**0.5)
+    MBerr = np.sum(((B-Bideal)**2)**0.5)
     
-    acc = accideal
+    B = Bideal
     
-Zs = Zs[H>0]
-acc = acc[H>0]
-q = q[H>0]
-S = S[H>0]   
-x = x[H>0]
-H = H[H>0]
 
-Zb = Zs-H
+Zb = h-H
 
 # Plot
 
 plt.xlabel("Distance [m]")
 plt.ylabel("Elevation [m]")
         
-plt.plot(x, Zs, 'k-', label='Ice surfae')
+plt.plot(x, h, 'k-', label='Ice surfae')
 plt.plot(x, Zb, 'k--', label='Bedrock')
 
 plt.legend(loc=1, fontsize=9, frameon=False)
